395ac6475df8c6bc84ae17e4de982870d6917ff2
DelKez <47643868+DelKez@users.noreply.github.com>
2023-12-20 02:13:46 +0100

    Testing

--- Projet/ts/index.ts
+++ Projet/ts/index.ts
@@ -1,13 +1,5 @@
-import { evaluate, unaryTest } from "feelin";
 import { App_Controller } from "./App_Controller";
 import { App_Model } from "./App_Model";
 import { App_View } from "./App_View";
 
 new App_Controller(new App_Model(), new App_View());
-
-function test() {
-  console.log("test():");
-  const evaluation: boolean|null = unaryTest("[13..65)", {'?': ['14']});
-  console.log(evaluation);
-}
-test();
